bf6292811c4d98d316796272cc5d63996db77619
Henriette Lehtonen <Henriette.Lehtonen@student.lab.fi>
2024-09-10 15:58:07 +0300

    update navbar + responsive

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const dialog = document.querySelector("dialog");
 const showButton = document.querySelector("span>i");
 const closeButton = document.querySelector(".close-btn");
+const hamburgerBtn = document.querySelector("#hamburgerBtn");
+const nav = document.querySelector(".top-menu");
+
+// Menu
+hamburgerBtn.addEventListener("click", hamburgerShow);
+
+function hamburgerShow() {
+  if (nav.className === "top-menu") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "top-menu";
+  }
+}
 
 // Avaa dialogin
 showButton.addEventListener("click", () => {
